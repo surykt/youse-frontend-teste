cbe9b2961835466f1d7ccafab4b0f3fa630c92b9
Carlos Alan <cardosocarlosalan@gmail.com>
2022-09-21 22:28:57 -0300

    add tailwind

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+
+import "./index.css";
+
 import App from "./components/App/App";
 
 const rootElement = document.getElementById("root");
