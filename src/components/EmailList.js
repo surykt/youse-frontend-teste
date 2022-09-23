0662ef076f64d709795d0937f3b6491f04a02543
Carlos Alan <cardosocarlosalan@gmail.com>
2022-09-23 11:55:37 -0300

    add style to EmailList component

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -4,10 +4,10 @@ export default function EmailList({ emails }) {
 
   return (<ul>
     {emails.map(email => (
-      <li key={email.id}>
+      <li className="border border-gray border-solid" key={email.id}>
         <article>
-          <h1>{email.title}</h1>
-          <p>{email.description}</p>
+          <h1 className="font-bold ml-10">{email.title}</h1>
+          <p className="ml-10">{email.description}</p>
         </article>
       </li>
     ))}
